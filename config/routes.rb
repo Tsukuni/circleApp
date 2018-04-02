92c8e0621550795bb27c899e3f80151bdac591fd
T_Kenta <tamkchi.fugu@gmail.com>
2018-04-02 19:08:53 +0900

    エラー処理

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   get 'adminpages/index'
   get 'advices/index'
   root to: 'home#index'
+  get '*path', controller: 'application', action: 'render_404'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
